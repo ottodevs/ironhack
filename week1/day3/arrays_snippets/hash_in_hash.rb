cbd0c78813adf2a7ef2640de8a96d5c3d7bd25cb
Otto G <ottodevs@gmail.com>
2016-10-25 14:53:24 +0200

    Removed submodules

---
+++ week1/day3/arrays_snippets/hash_in_hash.rb
@@ -0,0 +1,11 @@
+require "pry"
+
+lemonade_finances = {
+  monday:    { revenue: 20, cost: 5 },
+  tuesday:   { revenue: 15, cost: 4 },
+  wednesday: { revenue: 5,  cost: 3 },
+  thursday:  { revenue: 9,  cost: 3 },
+  friday:    { revenue: 17, cost: 4 },
+}
+
+binding.pry
