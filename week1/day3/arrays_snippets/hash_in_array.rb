ae51cb5a7db40479054b2414904da89f2b8b8e59
Otto G <ottodevs@gmail.com>
2016-10-25 14:53:24 +0200

    Removed submodules

---
+++ week1/day3/arrays_snippets/hash_in_array.rb
@@ -0,0 +1,10 @@
+require "pry"
+
+arr = [
+  { lat: 25, lng: 80, name: "Miami"    },
+  { lat: 48, lng: 2,  name: "Paris"    },
+  { lat: 40, lng: 3,  name: "Madrid"   },
+  { lat: 18, lng: 66, name: "San Juan" },
+]
+
+binding.pry
