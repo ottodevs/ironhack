4c9ce278fd153e2d14a2de4635b13e60f8cb928c
Otto G <ottodevs@gmail.com>
2016-11-02 19:44:36 +0100

    Refactor caesar's javascript

--- week3/day3/caesar_cipher/caesar_cipher.js
+++ week3/day3/caesar_cipher/caesar_cipher.js
@@ -1,18 +1,20 @@
 function caesarCipher (message, shift = -3) {
-  shift = shift > 26 ? shift % 26 : shift;
+  var a = 26; // from alphabet letters
+  var d = 97; // ascii displacemente
+  shift = Math.abs(shift) > a ? shift % a : shift;
 
   function translateChar(char) {
     var upper = char == char.toUpperCase();
-    var pos = char.toLowerCase().charCodeAt(0) + shift;
+    var pos = char.toLowerCase().charCodeAt(0) + shift - d;
 
-    if (char == char.toLowerCase()) {
-      if (pos > 122) {
-        pos -= 26;
-      } else if (pos < 97) {
-        pos += 26;
-      }
-    }
-    var theChar = String.fromCharCode(pos);
+      // if (pos 2) {
+      //   pos -= 26;
+      // } else if (pos < 97) {
+      //   pos += 26;
+      // }
+      // The above if-else shit elegantly becomes this:
+
+    var theChar = String.fromCharCode((pos + a) % a + d);
 
     return upper ? theChar.toUpperCase() : theChar;
   } // translateChar
@@ -25,6 +27,8 @@ function caesarCipher (message, shift = -3) {
 } // caesarCipher
 
 
+
+// Some tests:
 encrypted = caesarCipher("Et tu, brute?", 6);
 console.log(encrypted);
 //=> "Kz za, hxazk?"
@@ -34,3 +38,10 @@ console.log(encrypted);
 
 decrypted = caesarCipher("Kz za, hxazk?", -6);
 console.log(decrypted);
+
+theSentence = "Roses are red and violets are blue, I want to be an Ironhacker, what about you?"
+theShift = -34533240958324256259683240598324545;
+
+encrypted = caesarCipher(theSentence, theShift);
+decrypted = caesarCipher(encrypted, -theShift);
+console.log("Here you go!\n" + encrypted + "\n" + decrypted);
