6bca82c36d0fc4fa1d64a40ab0addaa11de37bb6
Otto G <ottodevs@gmail.com>
2016-11-14 18:13:19 +0100

    Polished pizza.js

--- week5/pizza-builder/src/pizza.js
+++ week5/pizza-builder/src/pizza.js
@@ -1,3 +1,5 @@
+/* global $ */
+
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function () {
   setupButtons()
@@ -11,11 +13,10 @@ function setupButtons () {
     $(buttons[i]).click(function () {
       $(this).toggleClass('active')
       var panelSelector = $(this).text().toLowerCase()
-      $(`.price>ul>li:contains(${panelSelector})`).toggle()
-      if (i < 3)
-        { $(`.${toppings[i]}`).toggle() }
-      else
-        { $(`.${toppings[i]}`).toggleClass(`${toppings[i + 2]}`) }
+      $(`.price>ul>li:contains(${panelSelector})`).fadeToggle(1000)
+      i < 3
+      ? $(`.${toppings[i]}`).toggle()
+      : $(`.${toppings[i]}`).toggleClass(`${toppings[i + 2]}`)
       updatePrice()
     })
   }
@@ -25,7 +26,7 @@ function updatePrice () {
   var price = 10
   var activeToppings = $('.controls>ul>li').children('.active')
   for (var i = 0; i < activeToppings.length; i++) {
-    price += parseInt($(activeToppings[i]).attr('data-price'))
+    price += parseInt($(activeToppings[i]).attr('data-price'), 10)
   }
   $('strong').text('$' + price)
 }
