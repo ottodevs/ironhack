f06a5b36d218918ec8d7a3830a028446d5199e95
Otto G <ottodevs@gmail.com>
2016-11-14 16:08:05 +0100

    Solved pizza builder exercise

--- week5/pizza-builder/src/pizza.js
+++ week5/pizza-builder/src/pizza.js
@@ -1,1 +1,47 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+  console.log('werwer')
+  updatePrice()
+  $('.btn').click(function () {
+    updatePrice()
+  })
+
+  $('.btn-pepperonni').click(function () {
+    $('.pep').toggle()
+    $('li:contains("pepperonni")').toggle()
+    $(this).toggleClass('active')
+  })
+
+  $('.btn-mushrooms').click(function () {
+    $('.mushroom').toggle()
+    $('li:contains("mushrooms")').toggle()
+    $('.btn-mushrooms').toggleClass('active')
+  })
+
+  $('.btn-green-peppers').click(function () {
+    $('.green-pepper').toggle()
+    $('li:contains("green peppers")').toggle()
+    $('.btn-green-peppers').toggleClass('active')
+  })
+
+  $('.btn-sauce').click(function () {
+    $('.sauce').toggleClass('sauce-white')
+    $('li:contains("white sauce")').toggle()
+    $('.btn-sauce').toggleClass('active')
+  })
+
+  $('.btn-crust').click(function () {
+    $('.crust').toggleClass('crust-gluten-free')
+    $('li:contains("gluten-free crust")').toggle()
+    $('.btn-crust').toggleClass('active')
+  })
+})
+
+function updatePrice () {
+  var prices = $('li:visible:contains("$")')
+  var price = 10
+  for (var i = 0; i < prices.length; i++) {
+    price += parseInt($(prices[i]).text()[1])
+  }
+  $('strong').text('$' + price)
+}
